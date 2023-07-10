5e8c8780d13fb3c0dac29fc8a350fba64a7418c8
rravech <139149855+rravech@users.noreply.github.com>
2023-07-10 14:37:52 -0400

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -4,8 +4,8 @@
 newheight = 100 # initial height
 i = 1  # number of times routine run
 while i < 11:
-    print(round(newheight*3/5))
-    newheight = newheight*3/5
-    i = i+1
+    print(newheight * 3/5)
+    newheight = newheight * 3/5
+    i = i + 1
 
 print(newheight)
