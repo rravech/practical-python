8640ba49c58378530aa34540b48e54dd1a165aea
rravech <139149855+rravech@users.noreply.github.com>
2023-07-11 09:35:55 -0400

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,10 +5,16 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+i = 1 #pmt number
 
 while principal > 0:
-  principal = principal * (1+rate/12) - payment
-  total_paid = total_paid + payment
+  if i < 13:
+      principal = principal * (1+rate/12) - (payment + 1000)
+      total_paid = total_paid + (payment - 1000)
+  else:
+      principal = principal * (1+rate/12) - payment
+      total_paid = total_paid + payment
+    i = i + 1
 
 print('Total paid',total_paid)
 print('Number of months',total_paid/2684.11)
