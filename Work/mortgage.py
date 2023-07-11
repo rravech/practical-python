2edb4977d7e3283ac418e424dee38362a482dfb7
rravech <139149855+rravech@users.noreply.github.com>
2023-07-11 12:13:47 -0400

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,9 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 i = 1 #pmt number
-extra_payment_start_month = input('Enter Month/Payment Number to start extra payment:') #61
-extra_payment_end_month = input('Enter Month Number/Payment Number to end extra payment:') #108
-extra_payment =input('Enter Extra Payment Dollar Amount:') #1000
+extra_payment_start_month = input('Enter Month/Payment Number to start extra payment:')
+extra_payment_end_month = input('Enter Month Number/Payment Number to end extra payment:')
+extra_payment =input('Enter Extra Payment Dollar Amount:')
 
 while principal > 0:
     if i >= int(extra_payment_start_month) and i <= int(extra_payment_end_month):
