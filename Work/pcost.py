b8fe2b800b064d85fe670536159491cba4f66a5b
rravech <139149855+rravech@users.noreply.github.com>
2023-07-12 16:38:24 -0400

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('portfolio.csv','rt')
+headers = next(f)
+headers
+shares_total = 0
+total_purchase_cost = 0
+price =
+for line in f:
+      shares_total = shares + shares_total
+      total_purchase_cost = total_purchase_cost + (shares*price)
